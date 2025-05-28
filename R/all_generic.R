af1f8d4deea811d5b91ab389f96a0177d723f7be
bbuchsbaum <brad.buchsbaum@gmail.com>
2025-05-27 22:20:56 -0400

    chore: clean up all_generic.R whitespace

--- R/all_generic.R
+++ R/all_generic.R
@@ -1,5 +1,3 @@
-
-
 if (!isGeneric("plot"))
   setGeneric("plot", function(x, y, ...) standardGeneric("plot"))
 
@@ -268,5 +266,3 @@ setGeneric(name = "surfwidget",
 setGeneric(name = "findBoundaries",
            def = function(x, method = "midpoint", ...)
              standardGeneric("findBoundaries"))
-
-
